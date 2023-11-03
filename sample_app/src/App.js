fa709009a5cd2ccc3c6acc24d4defeb92017744c
Shital B <bhagat@rezoomex.com>
2023-11-03 12:56:12 +0530

    enhanced text analyzer

--- sample_app/src/App.js
+++ sample_app/src/App.js
@@ -1,15 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-import Aboutus from './components/Aboutus';
+// import Aboutus from './components/Aboutus';
 import Navbar from './components/Navbar';
-// import Texform from './components/Texform';
+import Texform from './components/Texform';
 function App() {
   return (
     <div>
       <Navbar title="TextUtilsApp" about="About"></Navbar>
       <div className="container my-10">
-        {/* <Texform heading="Enter the text to analyze below"></Texform> */}
-        <Aboutus/>
+        <Texform heading="Enter the text to analyze below"></Texform>
+        {/* <Aboutus/> */}
       </div>
     </div>
   );
--- sample_app/src/components/Texform.js
+++ sample_app/src/components/Texform.js
@@ -5,33 +5,90 @@ import React, { useState } from 'react'
 export default function Texform(props) {
 
   const [text, setText] = useState("")
+  const [updatedText, setUpdatedText] = useState("")
+  // const [copyText, setCopytext] = useState("")
+  const [appearedWord, setAappearedWord] = useState("")
 
-  const toUpperCase = () =>{
-    setText(text.toUpperCase())
+  const handleUpperCase = () =>{
+    setUpdatedText(text.toUpperCase())
   }
 
-  const toLowerCase = () => {
-    setText(text.toLowerCase())
+  const handleLowerCase = () => {
+    setUpdatedText(text.toLowerCase())
   }
-  const onHandleChange = (event) => {
+
+  const handleClear = () => {
+    setText("")
+    setUpdatedText("")
+    setAappearedWord("")
+  }
+
+  const handleMostApprearedWord = () => {
+    let words= text.split(" ")
+    let maxCnt = 0
+    let mostApprearedWord = ""
+   
+    let counted= []
+    for(let i=0;i<words.length;i++) {
+      if(!counted.includes(words[i])) {
+        let cnt =1;
+        counted.push(words[i])
+        for(let j=i+1;j<words.length;j++) {
+          if(words[i]===words[j]) {
+            cnt++;
+          }
+        }
+        if(cnt>maxCnt) {
+          maxCnt = cnt;
+          mostApprearedWord  = words[i];
+        }
+      }
+    }
+    
+    console.log(mostApprearedWord)
+    setAappearedWord(mostApprearedWord)
+    // return {"word":mostApprearedWord, "cnt":macCnt}
+  }
+
+  const handleCopy = () => {
+    var copyText = document.getElementById('myBox')
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+  
+    alert("Copied the text: " + copyText.value);
+
+  }
+
+  const handleExtraSpaces = () => {
+    setUpdatedText(text.trim())
+  }
+
+  const handleOnChange = (event) => {
     setText(event.target.value)
-  };
+  }
+
   return (
     <>
         <div className="container my-4">
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8" value={text} onChange= {onHandleChange}></textarea>
+                <textarea className="form-control" id="myBox" rows="8" value={text} onChange= {handleOnChange}></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick={toUpperCase}>Upper Case</button>
-            <button className="btn btn-primary" onClick={toLowerCase}>Lower Case</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpperCase}>Upper Case</button>
+            <button className="btn btn-primary mx-2" onClick={handleLowerCase}>Lower Case</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button className="btn btn-primary mx-2" onClick={handleMostApprearedWord}>Most Appeared Word</button>
+            <button className="btn btn-primary mx-2" onClick={handleClear}>Clear</button>
         </div>
         <div className="container my-4">
             <h2>Text Analysis</h2>
             <p>{text.split(" ").length} Word count and {text.length} Character count</p>
             <p>{0.008 * text.split(" ").length} Minutes read</p>
+            {/* <p>Copy text: {copyText}</p> */}
+            <p>{appearedWord} is appeared</p>
             <h2>Text Preview</h2>
-            <p>{text}</p>
+            <p>{updatedText}</p>
         </div>
    </>
 
